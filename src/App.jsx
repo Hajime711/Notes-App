9bc5bd38c54d5aa4e67d5e03e9a7f14ef44aab05
Romaisa alvi <romaisa.uchiha@gmail.com>
2023-10-21 00:10:13 +0500

    setting up firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import React from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 
 export default function App() {
     const [notes, setNotes] = React.useState(
-        () => JSON.parse(localStorage.getItem("notes")) || []//Lazy state initialization
+        () => JSON.parse(localStorage.getItem("notes")) || []
     )
     const [currentNoteId, setCurrentNoteId] = React.useState(
-        (notes[0] && notes[0].id) || ""
+        (notes[0]?.id) || ""
     )
-    
+
+    const currentNote = 
+        notes.find(note => note.id === currentNoteId) 
+        || notes[0]
+
     React.useEffect(() => {
         localStorage.setItem("notes", JSON.stringify(notes))
     }, [notes])
-
+    
     function createNewNote() {
         const newNote = {
             id: nanoid(),
@@ -27,17 +30,24 @@ export default function App() {
     }
     
     function updateNote(text) {
-        setNotes(oldNotes => oldNotes.map(oldNote => {
-            return oldNote.id === currentNoteId
-                ? { ...oldNote, body: text }
-                : oldNote
-        }))
+        // Put the most recently-modified note at the top
+        setNotes(oldNotes => {
+            const newArray = []
+            for(let i = 0; i < oldNotes.length; i++) {
+                const oldNote = oldNotes[i]
+                if(oldNote.id === currentNoteId) {
+                    newArray.unshift({ ...oldNote, body: text })
+                } else {
+                    newArray.push(oldNote)
+                }
+            }
+            return newArray
+        })
     }
     
-    function findCurrentNote() {
-        return notes.find(note => {
-            return note.id === currentNoteId
-        }) || notes[0]
+    function deleteNote(event, noteId) {
+        event.stopPropagation()
+        setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
     }
     
     return (
@@ -52,15 +62,16 @@ export default function App() {
             >
                 <Sidebar
                     notes={notes}
-                    currentNote={findCurrentNote()}
+                    currentNote={currentNote}
                     setCurrentNoteId={setCurrentNoteId}
                     newNote={createNewNote}
+                    deleteNote={deleteNote}
                 />
                 {
                     currentNoteId && 
                     notes.length > 0 &&
                     <Editor 
-                        currentNote={findCurrentNote()} 
+                        currentNote={currentNote} 
                         updateNote={updateNote} 
                     />
                 }
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeyoDDUJK-x0R9wMJO99HHYVfqdb7kUtU",
+  authDomain: "react-notes-76202.firebaseapp.com",
+  projectId: "react-notes-76202",
+  storageBucket: "react-notes-76202.appspot.com",
+  messagingSenderId: "871717851889",
+  appId: "1:871717851889:web:e66d175704fbf5018acf50"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const notesCollection = collection(db, "notes")
